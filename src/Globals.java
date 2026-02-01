adef739021a4b7248dafcc309ca0c245e0288b2b
Gabriel <gabrielgrannas@gmail.com>
2026-02-01 20:57:04 +0100

    Globals.java have gotten a new variable for Reflector.java, this variable will be used as the standard value if the user dont want to change it. A simple function has also been added for getting a letters index in the alphabet. Since its a variable that will be needed in other upcoming functions.

--- src/Globals.java
+++ src/Globals.java
@@ -1,6 +1,7 @@
 package src;
 public class Globals {
     public static final int ALPHABET_SIZE = 26;
+    public static final int REFLECTOR_STANDARD_TRANSPOSITION = 13;
 
     // Legit wiring for enigma rotors, check en.wikipedia.org/wiki/Enigma_rotor_details
     public static final String[] WIRE_SETTING = {
@@ -8,4 +9,8 @@ public class Globals {
             "AJDKSIRUXBLHWTMCQGZNPYFVOE",   // Model: Enigma 1, Rotor# II
             "BDFHJLCPRTXVZNYEIWGAKMUSQO"    // Model: Enigma 1, Rotor# III
     };
+
+    public static int GET_LETTER_INDEX(char input) {
+        return input - 'A';
+    }
 }
